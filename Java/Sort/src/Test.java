3da4b4fcf5f389bcf3ed2a0d5bbff401c7446a8b
PatrickNg <PatrickNgZh@outlook.com>
2017-07-04 23:17:04 +0800

    add Test

---
+++ Java/Sort/src/Test.java
@@ -0,0 +1,14 @@
+
+public class Test {
+	public static void printArray(int[] array) {
+		for (int i = 0; i < array.length; i++) {
+			System.out.print(array[i]+" ");
+		}
+		System.out.println();
+	}
+	public static void main(String[] args) {
+		int[] array = new int[]{5,8,2,9,6,1,10,3};
+		Sort.selectSort(array);
+		Test.printArray(array);
+	}
+}
